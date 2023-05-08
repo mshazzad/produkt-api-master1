b75419c67120d4397767143bd9ebe45394e88559
Shazzad Hossain <m.shazzad@gmail.com>
2023-05-08 22:47:08 +0200

    test

---
+++ src/test/java/com/example/produktapi/repository/ProductRepositoryUnitTest.java
@@ -0,0 +1,19 @@
+package com.example.produktapi.repository;
+
+import com.example.produktapi.model.Product;
+import com.example.produktapi.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+public class ProductRepositoryUnitTest {
+
+    @Test
+    void test1() {
+
+    }
+}
