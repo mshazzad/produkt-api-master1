cfdd50e2c04bf854232836ebf95f7265271c08ba
Shazzad Hossain <m.shazzad@gmail.com>
2023-05-08 23:39:40 +0200

    test

--- src/test/java/com/example/produktapi/service/ProductServiceUnitTest.java
+++ src/test/java/com/example/produktapi/service/ProductServiceUnitTest.java
@@ -1,4 +1,13 @@
 package com.example.produktapi.service;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
 
+
+import java.util.List;
+
+
+@SpringBootTest
 public class ProductServiceUnitTest {
+
 }
